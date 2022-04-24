7655d28fb7edbc2c3e323a35051d98696d1f6e09
CheIIau <rip.rulez123@gmail.com>
2022-04-24 17:25:12 +0300

    DELETED: Unnecessary files

--- server/constants/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.jwtRefreshSecretKey = exports.jwtAccessSecretKey = exports.mongoUri = void 0;
-exports.mongoUri = 'mongodb+srv://CheIIau:KNMd2g4yAZqzw5x@clusterdb.6eqee.mongodb.net/savethatmoney?retryWrites=true&w=majority';
-exports.jwtAccessSecretKey = 'someKey';
-exports.jwtRefreshSecretKey = 'otherKey';
-//# sourceMappingURL=index.js.map
