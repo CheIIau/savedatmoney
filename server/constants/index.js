2b8f2d1522e343e88dff52edfbf69a628373706a
CheIIau <rip.rulez123@gmail.com>
2022-04-26 19:29:11 +0300

    UPDATE: added favicons and some other improvements

--- server/constants/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.jwtRefreshSecretKey = exports.jwtAccessSecretKey = exports.mongoUri = void 0;
-exports.mongoUri = process.env.MONGO_URI;
-exports.jwtAccessSecretKey = process.env.JWT_ACCESS_SECRET_KEY;
-exports.jwtRefreshSecretKey = process.env.JWT_REFRESH_SECRET_KEY;
-//# sourceMappingURL=index.js.map
